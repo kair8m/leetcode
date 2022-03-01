53174c979d6b30b2493508937296d9d440fbd6b7
kair8m <53782948+kair8m@users.noreply.github.com>
2022-03-01 19:07:37 +0300

    Time: 49 ms (10.14%), Space: 24 MB (5.11%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_set<int> set;
+        for (const auto& num : nums)
+            set.emplace(num);
+        for (int i = 0; i < nums.size(); ++i) {
+            if (set.find(i) == set.end())
+                return i;
+        }
+        return nums.size();
+    }
+};
