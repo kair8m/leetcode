214254278855a5158b51f45e0120344fe42eb563
kair8m <53782948+kair8m@users.noreply.github.com>
2022-03-13 01:42:57 +0300

    Time: 118 ms (77.67%), Space: 67.7 MB (54.67%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums, int left, int right) {
-        auto range = to_string(left) + '-' + to_string(right);
-        if (left > right)
-            return INT_MIN;
-        if (dp.find(range) != dp.end())
-            return dp[range];
-        int sum = 0;
-        for (int i = left; i <= right; ++i)
-            sum += nums[i];
-        int resultLeft = maxSubArray(nums, left + 1, right);
-        int resultRight = maxSubArray(nums, left, right - 1);
-        int result = max(resultLeft, resultRight);
-        int output = max(sum, result);
-        dp[range] = output;
-        return output;
-    }
-    
     int maxSubArray(vector<int>& nums) {
-        dp.clear();
-        
         int prefix = 0;
         int result = INT_MIN;
         for (int i = 0; i < nums.size(); ++i) {
@@ -32,8 +13,4 @@ public:
         
         return result;
     }
-    
-private:
-    unordered_map<string, int> dp;
-    
 };
