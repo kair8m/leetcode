e10ea9eda1301567bd8b6ff12397ee8798cfde5e
kair8m <53782948+kair8m@users.noreply.github.com>
2022-03-12 20:20:09 +0300

    Time: 13 ms (37.05%), Space: 7.9 MB (98.56%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -5,10 +5,13 @@ public:
         unordered_set<string> cache;
         for (int row = 0; row < board.size(); ++row) {
             for (int col = 0; col < board[row].size(); ++col) {
-                if (board[row][col] == 'X') {
-                    counter++;
-                    sink(board, row, col);
-                }
+                if (board[row][col] == '.')
+                    continue;
+                if (row > 0 && board[row - 1][col] == 'X')
+                    continue;
+                if (col > 0 && board[row][col - 1] == 'X')
+                    continue;
+                counter++;
             }
         }
         return counter;
