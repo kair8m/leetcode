efba6da756b336773dc4ab9bfe3dde954a4e889e
kair8m <53782948+kair8m@users.noreply.github.com>
2022-03-12 20:20:18 +0300

    Time: 18 ms (15.31%), Space: 8.2 MB (51.26%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -16,20 +16,4 @@ public:
         }
         return counter;
     }
-    
-    void sink(vector<vector<char>>& board, int r, int c) {
-        if (
-            r < 0 ||
-            c < 0 || 
-            r >= board.size() ||
-            c >= board[0].size() || 
-            board[r][c] != 'X'
-        )
-            return;
-        board[r][c] = '.';
-        sink(board, r + 1, c);
-        sink(board, r - 1, c);
-        sink(board, r, c + 1);
-        sink(board, r, c - 1);
-    }
 };
