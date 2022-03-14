8bcef59563b446856ebdd6dc930cf38c05c4c567
kair8m <53782948+kair8m@users.noreply.github.com>
2022-03-15 00:15:33 +0300

    Time: 797 ms (29.24%), Space: 7.9 MB (75.19%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,29 +1,6 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        unordered_set<string> cache;
-        unordered_map<char, int> lettersInWord, lettersInBoard;
-        
-        for (char c : word)
-            lettersInWord[c]++;
-        
-        for (int row = 0; row < board.size(); ++row) {
-            for (int col = 0; col < board[row].size(); ++col) {
-                lettersInBoard[board[row][col]]++;
-            }
-        }
-        
-        for (auto& entry : lettersInWord) {
-            printf("checking if board contains letter %c %d times\r\n", entry.first, entry.second);
-            int count = entry.second;
-            while(count--) {
-                if (lettersInBoard[entry.first]-- <= 0)
-                    return false;
-            }
-        }
-        
-        printf("checking if word exists...\r\n");
-        
         for (int row = 0; row < board.size(); ++row) {
             for (int col = 0; col < board[row].size(); ++col) {
                 if (exist(board, word, 0, row, col))
