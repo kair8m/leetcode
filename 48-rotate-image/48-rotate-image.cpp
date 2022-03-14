39e120aad3e296cb0de7801198a651844f042952
kair8m <53782948+kair8m@users.noreply.github.com>
2022-03-14 22:26:15 +0300

    Time: 3 ms (63.87%), Space: 7.1 MB (34.02%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -4,13 +4,11 @@ public:
         int diam = matrix.size();
         while (diam > 1) {
             int ptr = matrix.size() - diam;
-            printf("\r\nptr %d diam %d\r\n", ptr, diam);
             for (int i = 0; i + ptr < diam - 1; ++i) {
                 int val1 = matrix[ptr][i + ptr];
                 int val2 = matrix[i + ptr][diam - 1];
                 int val3 = matrix[diam - 1][diam - 1 - i];
                 int val4 = matrix[diam - 1 - i][ptr];
-                printf("val1 %d val2 %d val3 %d val4 %d\r\n", val1, val2, val3, val4);
                 matrix[ptr][i + ptr] = val4;
                 matrix[diam - 1 - i][ptr] = val3;
                 matrix[diam - 1][diam - 1 - i] =  val2;
