10223ff7af33edcd302b3d5c05baca890dca7cd8
kair8m <53782948+kair8m@users.noreply.github.com>
2022-04-04 11:13:09 +0300

    Time: 150 ms (29.17%), Space: 51.5 MB (49.55%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> cache;
-        for (int num : nums) {
+        for (int num : nums)
+        {
             if (cache.find(num) != cache.end())
                 return true;
             cache.emplace(num);
